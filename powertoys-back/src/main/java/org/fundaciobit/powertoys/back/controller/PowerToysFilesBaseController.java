41635070739c8e63126c20fca4f94a711b45ea09
Joan Pou (Fundació BIT) <jpou@fundaciobit.org>
2025-03-05 09:37:32 +0100

    soluciona error del revisor de codi "Redundant nullcheck of file"

--- powertoys-back/src/main/java/org/fundaciobit/powertoys/back/controller/PowerToysFilesBaseController.java
+++ powertoys-back/src/main/java/org/fundaciobit/powertoys/back/controller/PowerToysFilesBaseController.java
@@ -51,7 +51,7 @@ public abstract class PowerToysFilesBaseController<I extends IGenAppEntity, PK e
         }
       } catch (I18NException e) {
         log.error("Error esborrant arxiu fisic amb id=" + fileID +
-            ((file == null) ? "" : ("(" + file.getNom() + ")")) + ": " + I18NUtils.getMessage(e), e);
+            ("(" + file.getNom() + ")") + ": " + I18NUtils.getMessage(e), e);
       } catch (Exception e) {
         log.error("Error esborrant arxiu fisic amb id=" + fileID +
             ((file == null) ? "" : ("(" + file.getNom() + ")")) + ": " + e.getMessage(), e);
