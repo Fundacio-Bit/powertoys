bcda36541f09c33f3e364b602302bb7c3c0bbc09
Joan Pou (Fundació BIT) <jpou@fundaciobit.org>
2025-03-13 12:41:45 +0100

    actualitza esquema de creació de noves BBDD de compilació amb noms seguint convenció de genapp #24

--- scripts/bbdd/postgresql/1.0/update_from_1.0.1_to_1.0.2_schema.sql
+++ scripts/bbdd/postgresql/1.0/update_from_1.0.1_to_1.0.2_schema.sql
@@ -1,30 +1,40 @@
 create sequence pwt_repocompilacio_seq start 1000 increment 1;
 create sequence pwt_compilacio_seq start 1000 increment 1;
 
+
+-- public.pwt_repocompilacio definition
+
     create table pwt_repocompilacio (
-       repocompilacioid int8 not null,
+       repocompilacioid int8 default nextval('pwt_repocompilacio_seq'::regclass) not null,
         nom varchar(255) not null,
         organitzaciogithub varchar(255) not null,
         repositorigithub varchar(255) not null,
         ordre int8,
-        deshabilitat boolean not null,
-        primary key (repocompilacioid)
+        actiu boolean not null,
+        constraint pwt_repocompilacio_pk primary key (repocompilacioid)
     );
+create index pwt_repocompilacio_pk_i on pwt_repocompilacio (repocompilacioid);
+
+
+-- public.pwt_compilacio definition
 
     create table pwt_compilacio (
-       compilacioid int8 not null,
+       compilacioid int8 default nextval('pwt_compilacio_seq'::regclass) not null,
         repocompilacioid int8 not null,
         tagurl varchar(255) not null,
         datainici timestamp not null,
         datafi timestamp not null,
         exitcode int2 not null,
         output text,
-        primary key (compilacioid)
+        constraint pwt_compilacio_pk primary key (compilacioid)
     );
-create index pwt_repocompilacio_pk_i on pwt_repocompilacio (repocompilacioid);
 create index pwt_compilacio_pk_i on pwt_compilacio (compilacioid);
+create index pwt_compilacio_repocompid_fk_i on pwt_compilacio (repocompilacioid);
+
+
+-- pwt_compilacio foreign keys
 
     alter table pwt_compilacio 
-       add constraint pwt_compilacio_repocompilacio_fk 
+       add constraint pwt_compilacio_repocomp_rid_fk 
        foreign key (repocompilacioid) 
-       references pwt_repocompilacio;
+       references pwt_repocompilacio(repocompilacioid);
